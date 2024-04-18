cd3ea20a35e16f4898b373a7883a3c828839d2b8
Tecnofyle <shaymshukla807@gmail.com>
2024-04-18 18:42:31 +0530

    ToDo App using React

---
+++ Cohort/Todo Full/backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://admin:Yc2ZoqILvgAeAqn5@cluster0.uygtgfm.mongodb.net/My-todo-App";
+mongoose.connect(url);
+
+const todoSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+});
+
+const todo = new mongoose.model('todos', todoSchema)
+module.exports = {
+    todo
+}
